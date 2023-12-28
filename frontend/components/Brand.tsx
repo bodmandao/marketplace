5971588e6573dd9f922eff792ca98e5d1eeda4be
bodmandao <154741685+bodmandao@users.noreply.github.com>
2023-12-28 08:50:23 +0000

    brand section

---
+++ frontend/components/Brand.tsx
@@ -0,0 +1,17 @@
+function Brand(){
+    return (
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-md-6">
+
+                </div>
+                
+                <div className="col-md-6" style={{"backgroundImage" : "url('')"}}>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Brand
