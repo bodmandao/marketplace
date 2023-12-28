00a017ec957ea206d9708dc750056b729fedea72
bodmandao <154741685+bodmandao@users.noreply.github.com>
2023-12-28 20:14:04 +0000

    brand section

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import Navbar from "../components/Navbar"
+import Brand from "../components/Brand"
 
 export default function Home() {
   return (
       <main>
           <Navbar />
+          <Brand />
       </main>
   )
 }
--- frontend/components/Brand.tsx
+++ frontend/components/Brand.tsx
@@ -1,12 +1,12 @@
 function Brand(){
     return (
         <div className="container-fluid">
-            <div className="row">
-                <div className="col-md-6">
-
+            <div className="row" style={{"height":"50vh"}}>
+                <div className="col-md-6 text-left p-3" style={{"backgroundColor":"bisque"}}>
+                    <h2 className="text-secondary" style={{"position":"relative","top":"25%"}}>Buy and sell with the most secured decentralized website</h2>
                 </div>
                 
-                <div className="col-md-6" style={{"backgroundImage" : "url('')"}}>
+                <div className="col-md-6" style={{"backgroundImage" : "url('/e-commerce-online.avif')"}}>
 
                 </div>
             </div>
