41cedfb3171c9638d7d97fc1b75a6720b9e8494a
bodmandao <154741685+bodmandao@users.noreply.github.com>
2023-12-28 07:05:21 +0000

    transactions iterfaces

---
+++ frontend/app/interfaces/freelancerForm.ts
@@ -0,0 +1,8 @@
+interface FreelancerForm{
+    name : string | undefined,
+    country : string | undefined,
+    skills : string | undefined,
+    imageURL : string | undefined
+}
+
+export default FreelancerForm
